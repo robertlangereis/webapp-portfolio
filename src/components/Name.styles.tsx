c1f051ab65b6042e88722a81cb636f62c62e7235
robert <robert.langereis@gmail.com>
2022-01-22 14:49:39 +0100

    feature/name-component-setup: every transition/animation working as it should; significant refactoring needed

--- src/components/Name.styles.tsx
+++ src/components/Name.styles.tsx
@@ -1,6 +1,16 @@
 import { makeStyles } from '@mui/styles'
 
 export const useStyles = makeStyles({
+	hide: { color: '#000', transition: '1s linear' },
+	hideInstant: { opacity: 0, color: '#000',  animation: `$fadeOut linear 0s`, },
+	marginLeft: {marginLeft:'222px'},
+	none: { display: 'none' },
+	delayVisible: {
+		opacity: 0,
+		transitionDelay: '1000',
+		animationDelay: '1000',
+		display: 'none',
+	},
 	nameElement: {
 		color: '#484848',
 		fontSize: '50px',
@@ -11,22 +21,53 @@ export const useStyles = makeStyles({
 	},
 	spanElement: {
 		transition: '1s linear',
-		marginLeft: '0px',
 	},
 	about: {
-		marginLeft: '0px',
 		color: '#000',
 	},
-	aboutElements: {
+	skills: {
 		transition: '1s linear',
-		marginLeft: '5px',
+		color: '#fff',
+		textShadow: '0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff',
+		animation: '$fadeIn linear 1s',
+	},
+	letterS: {
+		transition: '1s ease-in-out',
+		color: '#fff',
+		textShadow: '0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff',
+		animation: '$fadeIn linear 1s',
+	},
+	fadeOut: {
+		color: '#484848',
+		animation: `$fadeOut linear 1s`,
+		opacity: 0,
+	},
+	'@keyframes fadeOut': {
+		'0%': { opacity: 1 },
+		'100%': { opacity: 0 },
+	},
+	fadeIn: {
+		animation: `$fadeIn linear 1s`,
+		opacity: 1,
+	},
+	BLa:{
+		opacity: 1,
+		animation: `$fadeIn linear 1s`,
 		color: '#fff',
 		textShadow: '0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff',
 	},
-	langereisElement: {
+	'@keyframes fadeIn': {
+		'0%': { opacity: 0 },
+		'25%': { opacity: 0.1 },
+		'50%': { opacity: 0.4 },
+		'100%': { opacity: 1 },
+	},
+	aboutElements: {
 		transition: '1s linear',
-		marginLeft: '0px',
-		color: '#000',
+		opacity: 1,
+		display: 'block',
+		color: '#fff',
+		textShadow: '0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff',
 	},
 })
 
--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -5,40 +5,149 @@ import useStyles from './Name.styles.tsx'
 export const Name: React.FC = () => {
 	const classes = useStyles()
 	const [aboutActive, setAboutActive] = useState(false)
+	const [skillsActive, setSkillsActive] = useState(false)
+	const [codeActive, setCodeActive] = useState(false)
+	const [contactActive, setContactActive] = useState(false)
+
+	const classSelector = (element: string, elNum?: number): string => {
+		const classNames = []
+		switch (element) {
+			case 'about':
+				if (aboutActive) classNames.push(classes.aboutElements)
+				else classNames.push(classes.about)
+				break
+			case 'Robert':
+				if (aboutActive) classNames.push(classes.aboutElements)
+				if (skillsActive) classNames.push(classes.skills)
+				if (contactActive) classNames.push(classes.fadeOut, classes.hide)
+				else classNames.push(classes.spanElement)
+				break
+			case 'letterS':
+				if (aboutActive || contactActive) classNames.push(classes.hide)
+				if (skillsActive) classNames.push(classes.skills)
+				if (contactActive) classNames.push(classes.hide)
+				else classNames.push(classes.spanElement, classes.fadeIn)
+				break
+			case 'RobertS':
+				if (aboutActive) classNames.push(classes.hide)
+				if (skillsActive) classNames.push(classes.skills, classes.fadeIn)
+				else classNames.push(classes.hide)
+				break
+			case 'ngerei':
+				if (aboutActive) classNames.push(classes.hide)
+				if (skillsActive) classNames.push(classes.none)
+				if (contactActive) classNames.push(classes.fadeOut, classes.hide)
+				else classNames.push(classes.spanElement, classes.fadeIn)
+				break
+			case 'La':
+				if (aboutActive) classNames.push(classes.hide)
+				if (skillsActive) classNames.push(classes.none)
+				if (contactActive)
+					classNames.push(classes.fadeIn, classes.aboutElements)
+				else classNames.push(classes.spanElement, classes.fadeIn)
+				break
+			case 'skills':
+				if (skillsActive) classNames.push(classes.skills, classes.fadeIn)
+				else classNames.push(classes.hideInstant)
+				break
+			case 'B':
+				if (contactActive)
+					classNames.push(classes.fadeIn, classes.aboutElements)
+				else classNames.push(classes.hide)
+				break
+			case 'BLa':
+				if (contactActive) classNames.push(classes.fadeIn)
+				else classNames.push(classes.hide)
+				break
+			case 'move':
+				if (skillsActive) classNames.push(classes.marginLeft)
+				break
+			case '<':
+				if (codeActive) classNames.push(classes.fadeIn, classes.aboutElements)
+				if (skillsActive)
+					classNames.push(classes.hideInstant)
+				else classNames.push(classes.hide)
+				break
+			case '/>':
+				if (codeActive) classNames.push(classes.fadeIn, classes.aboutElements)
+				if (skillsActive)
+					classNames.push(classes.hideInstant) && classNames.push(classes.none)
+				else classNames.push(classes.hide)
+				break
+			default:
+				break
+		}
+		return classNames.join(' ')
+	}
 	return (
-		<div className='bg-primary-black flex mx-auto w-screen h-screen my-0 p-5'>
+		<div className='bg-primary-black flex mx-auto w-screen h-screen my-0'>
 			<button
-				className='bg-white relative top-0 left-1/2 w-1/12 rounded-full h-10'
+				className='bg-white absolute top-0 w-1/12 mx-auto left-0 right-0 rounded-full h-10'
 				onClick={() => setAboutActive(!aboutActive)}
 			>
-				CLICK
+				About
+			</button>
+			<button
+				className='bg-white absolute top-0 bottom-0 my-auto w-1/12 right-0 rounded-full h-10'
+				onClick={() => setContactActive(!contactActive)}
+			>
+				Contact
+			</button>
+
+			<button
+				className='bg-white absolute bottom-0 w-1/12 mx-auto left-0 right-0 rounded-full h-10'
+				onClick={() => setSkillsActive(!skillsActive)}
+			>
+				Skills
+			</button>
+			<button
+				className='bg-white absolute top-0 bottom-0 my-auto w-1/12 left-0 rounded-full h-10'
+				onClick={() => setCodeActive(!codeActive)}
+			>
+				Code
 			</button>
 			<div className='flex w-full h-full justify-center  items-center'>
-				<h1
+				<div
 					className={`${classes.nameElement} flex flex-col items-center font-bold text-3xl`}
 				>
-					<span
-						className={`${
-							aboutActive ? classes.aboutElements : classes.about
-						} mr-7`}
-					>
-						about
-					</span>
-					<span
-						className={`${
-							aboutActive ? classes.aboutElements : classes.spanElement
-						} pb-2`}
-					>
-						Robert
-					</span>
-					<span
-						className={`${
-							aboutActive ? classes.langereisElement : classes.spanElement
-						} pt-2`}
-					>
-						Langereis
-					</span>
-				</h1>
+					<span className={`${classSelector('about')} mr-7`}>about</span>
+					<div className='flex flex-row'>
+						<span className={`${classSelector('<')} pb-2`}>{'<'}</span>
+						<span className={`${classSelector('Robert')} pb-2`}>Robert</span>
+						<span className={`${classSelector('RobertS')} pb-2`}>s</span>
+					</div>
+					<div className={`${classSelector('move')} flex flex-row pl-64`}>
+						<span className={`${classSelector('B')} pt-2`}>B</span>
+						<span className={`${classSelector('La')} pt-2`}>La</span>
+						<span className={`${classSelector('ngerei')} pt-2`}>ngerei</span>
+						<span className={`${classSelector('letterS')} pt-2`}>s</span>
+						<span className={`${classSelector('/>')} pt-2`}>{'/>'}</span>
+						<span className={`${classSelector('skills')} pt-2`}>kills</span>
+					</div>
+					<div className='flex flex-col w-full pt-2 pl-64'>
+						<span
+							className={`${classSelector(
+								'BLa'
+							)} transition delay-500 ease-in-out pt-2 pl-32`}
+						>
+							BLa
+						</span>
+						<span
+							className={`${classSelector(
+								'BLa'
+							)} transition delay-700 ease-in-out pt-2 pl-64`}
+						>
+							BLa
+						</span>
+						<span
+							className={`${classSelector(
+								'BLa'
+							)} transition delay-1000 ease-in-out pt-2 pl-96`}
+						>
+							BLa
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
