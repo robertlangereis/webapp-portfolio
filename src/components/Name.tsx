3bc8770418bc641d904ab3998e9a4178e8305479
robert <robert.langereis@gmail.com>
2022-01-30 13:03:25 +0100

    feature/name-component-setup: refactored the active states using only one useState inputSelector with string

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,75 +1,83 @@
 import React, { useState } from 'react'
 // @ts-ignore
 import useStyles from './Name.styles.tsx'
+// @ts-ignore
+import { Quote } from './Quote.tsx'
 
 export const Name: React.FC = () => {
 	const classes = useStyles()
-	const [aboutActive, setAboutActive] = useState(false)
-	const [skillsActive, setSkillsActive] = useState(false)
-	const [codeActive, setCodeActive] = useState(false)
-	const [contactActive, setContactActive] = useState(false)
+	const [inputActive, setInputActive] = useState('')
 
-	const classSelector = (element: string, elNum?: number): string => {
+	const classSelector = (element: string): string => {
 		const classNames = []
 		switch (element) {
 			case 'about':
-				if (aboutActive) classNames.push(classes.aboutElements)
+				if (inputActive === 'aboutActive')
+					classNames.push(classes.aboutElements)
 				else classNames.push(classes.about)
 				break
 			case 'Robert':
-				if (aboutActive) classNames.push(classes.aboutElements)
-				if (skillsActive) classNames.push(classes.skills)
-				if (contactActive) classNames.push(classes.fadeOut, classes.hide)
+				if (inputActive === 'aboutActive')
+					classNames.push(classes.aboutElements)
+				if (inputActive === 'skillsActive') classNames.push(classes.skills)
+				if (inputActive === 'contactActive')
+					classNames.push(classes.fadeOut, classes.hide)
 				else classNames.push(classes.spanElement)
 				break
 			case 'letterS':
-				if (aboutActive || contactActive) classNames.push(classes.hide)
-				if (skillsActive) classNames.push(classes.skills)
-				if (contactActive) classNames.push(classes.hide)
+				if (inputActive === 'contactActive' || inputActive === 'aboutActive')
+					classNames.push(classes.hide)
+				if (inputActive === 'skillsActive') classNames.push(classes.skills)
+				if (inputActive === 'contactActive') classNames.push(classes.hide)
 				else classNames.push(classes.spanElement, classes.fadeIn)
 				break
 			case 'RobertS':
-				if (aboutActive) classNames.push(classes.hide)
-				if (skillsActive) classNames.push(classes.skills, classes.fadeIn)
+				if (inputActive === 'aboutActive') classNames.push(classes.hide)
+				if (inputActive === 'skillsActive')
+					classNames.push(classes.skills, classes.fadeIn)
 				else classNames.push(classes.hide)
 				break
 			case 'ngerei':
-				if (aboutActive) classNames.push(classes.hide)
-				if (skillsActive) classNames.push(classes.none)
-				if (contactActive) classNames.push(classes.fadeOut, classes.hide)
+				if (inputActive === 'aboutActive') classNames.push(classes.hide)
+				if (inputActive === 'skillsActive') classNames.push(classes.none)
+				if (inputActive === 'contactActive')
+					classNames.push(classes.fadeOut, classes.hide)
 				else classNames.push(classes.spanElement, classes.fadeIn)
 				break
 			case 'La':
-				if (aboutActive) classNames.push(classes.hide)
-				if (skillsActive) classNames.push(classes.none)
-				if (contactActive)
+				if (inputActive === 'aboutActive') classNames.push(classes.hide)
+				if (inputActive === 'skillsActive') classNames.push(classes.none)
+				if (inputActive === 'contactActive')
 					classNames.push(classes.fadeIn, classes.aboutElements)
 				else classNames.push(classes.spanElement, classes.fadeIn)
 				break
 			case 'skills':
-				if (skillsActive) classNames.push(classes.skills, classes.fadeIn)
+				if (inputActive === 'skillsActive')
+					classNames.push(classes.skills, classes.fadeIn)
 				else classNames.push(classes.hideInstant)
 				break
 			case 'B':
-				if (contactActive)
+				if (inputActive === 'contactActive')
 					classNames.push(classes.fadeIn, classes.aboutElements)
 				else classNames.push(classes.hide)
 				break
 			case 'BLa':
-				if (contactActive) classNames.push(classes.fadeIn)
+				if (inputActive === 'contactActive') classNames.push(classes.fadeIn)
 				else classNames.push(classes.hide)
 				break
 			case 'move':
-				if (skillsActive) classNames.push(classes.marginLeft)
+				if (inputActive === 'skillsActive') classNames.push(classes.marginLeft)
 				break
 			case '<':
-				if (codeActive) classNames.push(classes.fadeIn, classes.aboutElements)
-				if (skillsActive) classNames.push(classes.hideInstant)
+				if (inputActive === 'codeActive')
+					classNames.push(classes.fadeIn, classes.aboutElements)
+				if (inputActive === 'skillsActive') classNames.push(classes.hideInstant)
 				else classNames.push(classes.hide)
 				break
 			case '/>':
-				if (codeActive) classNames.push(classes.fadeIn, classes.aboutElements)
-				if (skillsActive)
+				if (inputActive === 'codeActive')
+					classNames.push(classes.fadeIn, classes.aboutElements)
+				if (inputActive === 'skillsActive')
 					classNames.push(classes.hideInstant) && classNames.push(classes.none)
 				else classNames.push(classes.hide)
 				break
@@ -80,36 +88,46 @@ export const Name: React.FC = () => {
 	}
 	return (
 		<div className='bg-primary-black flex mx-auto w-screen h-screen my-0'>
-			<button
-				className='bg-white absolute top-0 w-1/12 mx-auto left-0 right-0 rounded-full h-10'
-				onClick={() => setAboutActive(!aboutActive)}
+			<h2
+				className={`${(inputActive === 'codeActive' ||
+					inputActive === 'contactActive' ||
+					inputActive === 'skillsActive') &&
+					classes.hide} typewriter text-opacity-50 from-shade-darkest transition ease-in-out absolute text-primary-lightest top-20 cursor-pointer text-4xl w-max text-center top-0 mx-auto left-0 right-0 h-10 font-extrabold font-mono`}
+				onClick={() => setInputActive('aboutActive')}
 			>
 				About
-			</button>
-			<button
-				className='bg-white absolute top-0 bottom-0 my-auto w-1/12 right-0 rounded-full h-10'
-				onClick={() => setContactActive(!contactActive)}
+			</h2>
+			<h2
+				className={`${(inputActive === 'codeActive' ||
+					inputActive === 'skillsActive' ||
+					inputActive === 'aboutActive') &&
+					classes.hide} typewriter text-opacity-50 from-shade-darkest transition ease-in-out text-primary-lightest cursor-pointer text-4xl w-max text-center mx-auto h-10 font-extrabold font-mono
+				absolute top-0 bottom-0 my-auto right-20`}
+				onClick={() => setInputActive('contactActive')}
 			>
 				Contact
-			</button>
+			</h2>
 
 			<h2
-				className={`${skillsActive &&
-					classes.hide} arrow-down text-opacity-50 from-shade-darkest transition ease-in-out absolute text-primary-lightest bottom-20 cursor-pointer text-5xl w-max text-center mx-auto left-0 right-0 h-10 font-extrabold font-mono`}
-				onClick={() => setSkillsActive(!skillsActive)}
+				className={`${(inputActive === 'codeActive' ||
+					inputActive === 'contactActive' ||
+					inputActive === 'aboutActive') &&
+					classes.hide} typewriter text-opacity-50 from-shade-darkest transition ease-in-out absolute text-primary-lightest bottom-20 cursor-pointer text-4xl w-max text-center mx-auto left-0 right-0 h-10 font-extrabold font-mono`}
+				onClick={() => setInputActive('skillsActive')}
 			>
 				skills
 				<span></span>
-				<span></span>
-				<span></span>
 			</h2>
 			<div className='absolute bottom-0 left-0 right-0'></div>
-			<button
-				className='bg-white absolute top-0 bottom-0 my-auto w-1/12 left-0 rounded-full h-10'
-				onClick={() => setCodeActive(!codeActive)}
+			<h2
+				className={`${(inputActive === 'skillsActive' ||
+					inputActive === 'contactActive' ||
+					inputActive === 'aboutActive') &&
+					classes.hide} typewriter text-opacity-50 from-shade-darkest transition ease-in-out absolute text-primary-lightest top-0 bottom-0 cursor-pointer w-max text-center my-auto left-20 h-10 font-extrabold font-mono`}
+				onClick={() => setInputActive('codeActive')}
 			>
-				Code
-			</button>
+				Coding
+			</h2>
 			<div className='flex w-full h-full justify-center  items-center'>
 				<div
 					className={`${classes.nameElement} flex flex-col items-center font-bold text-3xl`}
@@ -151,6 +169,7 @@ export const Name: React.FC = () => {
 							BLa
 						</span>
 					</div>
+					{inputActive && <Quote inputActive={inputActive} />}
 				</div>
 			</div>
 		</div>
---
+++ src/components/Quote.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export type QuoteProps = {
+	inputActive: string
+}
+
+export const Quote: React.FC<QuoteProps> = ({ inputActive }) => {
+	let quoteBlock
+	switch (inputActive) {
+		case 'codeActive':
+			quoteBlock = 'the process or activity of writing computer programs'
+			break
+		case 'skillsActive':
+			quoteBlock = 'the ability to do something well; expertise'
+			break
+		case 'aboutActive':
+			quoteBlock = 'on the subject of; concerning'
+			break
+		case 'contactActive':
+			quoteBlock =
+				'communicate with someone, typically in order to give or receive information'
+			break
+		default:
+			break
+	}
+	return (
+		<div className='typewriter pt-20'>
+			<h3 className='overflow-hidden italic'> {quoteBlock}</h3>
+		</div>
+	)
+}
