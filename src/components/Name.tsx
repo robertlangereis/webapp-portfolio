c70a5c5d05c947f223d34d5d9fe869db6994753b
robert <robert.langereis@gmail.com>
2022-01-30 13:08:18 +0100

    feature/name-component-setup: moving the classSelector function to seperate file

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 // @ts-ignore
+import { classSelector } from './ClassSelector.ts'
+// @ts-ignore
 import useStyles from './Name.styles.tsx'
 // @ts-ignore
 import { Quote } from './Quote.tsx'
@@ -7,85 +9,6 @@ import { Quote } from './Quote.tsx'
 export const Name: React.FC = () => {
 	const classes = useStyles()
 	const [inputActive, setInputActive] = useState('')
-
-	const classSelector = (element: string): string => {
-		const classNames = []
-		switch (element) {
-			case 'about':
-				if (inputActive === 'aboutActive')
-					classNames.push(classes.aboutElements)
-				else classNames.push(classes.about)
-				break
-			case 'Robert':
-				if (inputActive === 'aboutActive')
-					classNames.push(classes.aboutElements)
-				if (inputActive === 'skillsActive') classNames.push(classes.skills)
-				if (inputActive === 'contactActive')
-					classNames.push(classes.fadeOut, classes.hide)
-				else classNames.push(classes.spanElement)
-				break
-			case 'letterS':
-				if (inputActive === 'contactActive' || inputActive === 'aboutActive')
-					classNames.push(classes.hide)
-				if (inputActive === 'skillsActive') classNames.push(classes.skills)
-				if (inputActive === 'contactActive') classNames.push(classes.hide)
-				else classNames.push(classes.spanElement, classes.fadeIn)
-				break
-			case 'RobertS':
-				if (inputActive === 'aboutActive') classNames.push(classes.hide)
-				if (inputActive === 'skillsActive')
-					classNames.push(classes.skills, classes.fadeIn)
-				else classNames.push(classes.hide)
-				break
-			case 'ngerei':
-				if (inputActive === 'aboutActive') classNames.push(classes.hide)
-				if (inputActive === 'skillsActive') classNames.push(classes.none)
-				if (inputActive === 'contactActive')
-					classNames.push(classes.fadeOut, classes.hide)
-				else classNames.push(classes.spanElement, classes.fadeIn)
-				break
-			case 'La':
-				if (inputActive === 'aboutActive') classNames.push(classes.hide)
-				if (inputActive === 'skillsActive') classNames.push(classes.none)
-				if (inputActive === 'contactActive')
-					classNames.push(classes.fadeIn, classes.aboutElements)
-				else classNames.push(classes.spanElement, classes.fadeIn)
-				break
-			case 'skills':
-				if (inputActive === 'skillsActive')
-					classNames.push(classes.skills, classes.fadeIn)
-				else classNames.push(classes.hideInstant)
-				break
-			case 'B':
-				if (inputActive === 'contactActive')
-					classNames.push(classes.fadeIn, classes.aboutElements)
-				else classNames.push(classes.hide)
-				break
-			case 'BLa':
-				if (inputActive === 'contactActive') classNames.push(classes.fadeIn)
-				else classNames.push(classes.hide)
-				break
-			case 'move':
-				if (inputActive === 'skillsActive') classNames.push(classes.marginLeft)
-				break
-			case '<':
-				if (inputActive === 'codeActive')
-					classNames.push(classes.fadeIn, classes.aboutElements)
-				if (inputActive === 'skillsActive') classNames.push(classes.hideInstant)
-				else classNames.push(classes.hide)
-				break
-			case '/>':
-				if (inputActive === 'codeActive')
-					classNames.push(classes.fadeIn, classes.aboutElements)
-				if (inputActive === 'skillsActive')
-					classNames.push(classes.hideInstant) && classNames.push(classes.none)
-				else classNames.push(classes.hide)
-				break
-			default:
-				break
-		}
-		return classNames.join(' ')
-	}
 	return (
 		<div className='bg-primary-black flex mx-auto w-screen h-screen my-0'>
 			<h2
@@ -132,23 +55,91 @@ export const Name: React.FC = () => {
 				<div
 					className={`${classes.nameElement} flex flex-col items-center font-bold text-3xl`}
 				>
-					<span className={`${classSelector('about')} mr-7`}>about</span>
+					<span
+						className={`${classSelector(classes, inputActive, 'about')} mr-7`}
+					>
+						about
+					</span>
 					<div className='flex flex-row'>
-						<span className={`${classSelector('<')} pb-2`}>{'<'}</span>
-						<span className={`${classSelector('Robert')} pb-2`}>Robert</span>
-						<span className={`${classSelector('RobertS')} pb-2`}>s</span>
+						<span
+							className={`${classSelector(classes, inputActive, '<')} pb-2`}
+						>
+							{'<'}
+						</span>
+						<span
+							className={`${classSelector(
+								classes,
+								inputActive,
+								'Robert'
+							)} pb-2`}
+						>
+							Robert
+						</span>
+						<span
+							className={`${classSelector(
+								classes,
+								inputActive,
+								'RobertS'
+							)} pb-2`}
+						>
+							s
+						</span>
 					</div>
-					<div className={`${classSelector('move')} flex flex-row pl-64`}>
-						<span className={`${classSelector('B')} pt-2`}>B</span>
-						<span className={`${classSelector('La')} pt-2`}>La</span>
-						<span className={`${classSelector('ngerei')} pt-2`}>ngerei</span>
-						<span className={`${classSelector('letterS')} pt-2`}>s</span>
-						<span className={`${classSelector('/>')} pt-2`}>{'/>'}</span>
-						<span className={`${classSelector('skills')} pt-2`}>kills</span>
+					<div
+						className={`${classSelector(
+							classes,
+							inputActive,
+							'move'
+						)} flex flex-row pl-64`}
+					>
+						<span
+							className={`${classSelector(classes, inputActive, 'B')} pt-2`}
+						>
+							B
+						</span>
+						<span
+							className={`${classSelector(classes, inputActive, 'La')} pt-2`}
+						>
+							La
+						</span>
+						<span
+							className={`${classSelector(
+								classes,
+								inputActive,
+								'ngerei'
+							)} pt-2`}
+						>
+							ngerei
+						</span>
+						<span
+							className={`${classSelector(
+								classes,
+								inputActive,
+								'letterS'
+							)} pt-2`}
+						>
+							s
+						</span>
+						<span
+							className={`${classSelector(classes, inputActive, '/>')} pt-2`}
+						>
+							{'/>'}
+						</span>
+						<span
+							className={`${classSelector(
+								classes,
+								inputActive,
+								'skills'
+							)} pt-2`}
+						>
+							kills
+						</span>
 					</div>
 					<div className='flex flex-col w-full pt-2 pl-64'>
 						<span
 							className={`${classSelector(
+								classes,
+								inputActive,
 								'BLa'
 							)} transition delay-500 ease-in-out pt-2 pl-32`}
 						>
@@ -156,6 +147,8 @@ export const Name: React.FC = () => {
 						</span>
 						<span
 							className={`${classSelector(
+								classes,
+								inputActive,
 								'BLa'
 							)} transition delay-700 ease-in-out pt-2 pl-64`}
 						>
@@ -163,6 +156,8 @@ export const Name: React.FC = () => {
 						</span>
 						<span
 							className={`${classSelector(
+								classes,
+								inputActive,
 								'BLa'
 							)} transition delay-1000 ease-in-out pt-2 pl-96`}
 						>
